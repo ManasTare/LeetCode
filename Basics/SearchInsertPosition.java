7817f0de6fa73841e12ea633c51f602e5d410d80
MagnusAustin <manastare27@gmail.com>
2024-03-26 14:37:24 +0530

    Second commit

--- Basics/SearchInsertPosition.java
+++ Basics/SearchInsertPosition.java
@@ -1,7 +1,7 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int k=0;
-        int max=0;
+        //int max=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
                 k=i;
