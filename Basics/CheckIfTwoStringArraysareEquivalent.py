344c851c4a455c690f3cd029c79551c3c08732cd
Manas Tare <111227859+MagnusAustin@users.noreply.github.com>
2024-04-13 18:26:34 +0530

    Create CheckIfTwoStringArraysareEquivalent.py

---
+++ Basics/CheckIfTwoStringArraysareEquivalent.py
@@ -0,0 +1,10 @@
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        res1 = ""
+        res2 = ""
+        for x,y in word1,wordq:
+            res1 = res1 + x
+        for x in word2:
+            res2 = res2 + x
+        return (res1==res2)
+        
