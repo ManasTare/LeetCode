889c02f03e34ad274e6effce23325b8646052404
Manas Tare <111227859+MagnusAustin@users.noreply.github.com>
2024-04-14 11:27:01 +0530

    Update NumberofLaserBeamsinaBank.py

--- Basics/NumberofLaserBeamsinaBank.py
+++ Basics/NumberofLaserBeamsinaBank.py
@@ -1,7 +1,6 @@
-# 142 / 147 testcases passed
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        for x in bank:
+        for x in bank.copy():
             if(x.count("1")==0):
                 bank.remove(x)
         res = 0
@@ -11,4 +10,3 @@ class Solution:
             else:
                 res = res + bank[i].count("1")*bank[i+1].count("1")   
         return res
-        
